f572ef5d9a62195d3a40ab52e563596809cfbb09
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2015-02-10 13:40:23 +0000

    Update appmodule.js

--- appmodule.js
+++ appmodule.js
@@ -1,1 +1,12 @@
+// RequestAnimFrame: a browser API for getting smooth animations
+window.requestAnimFrame = (function () {
+    return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (callback) {
+        return window.setTimeout(callback, 1000 / 60);
+    };
+})();
+
+window.cancelRequestAnimFrame = (function () {
+    return window.cancelAnimationFrame || window.webkitCancelRequestAnimationFrame || window.mozCancelRequestAnimationFrame || window.oCancelRequestAnimationFrame || window.msCancelRequestAnimationFrame || clearTimeout
+})();
+
 var myApp = angular.module('app', []);
