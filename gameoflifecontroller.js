eb734476088891d266eac82635df125fa91ba37d
Michal Paszkiewicz <michalpaszkiewicz@hotmail.com>
2015-02-11 10:51:31 +0000

    Update gameoflifecontroller.js

--- gameoflifecontroller.js
+++ gameoflifecontroller.js
@@ -21,7 +21,8 @@ angular.module('app').controller('gameoflifeController', function($scope){
   $scope.races = [
     {reproduction: 3, overpopulation: 4, damage: 1, colour: "yellow"},
     {reproduction: 3, overpopulation: 4, damage: 0, colour: "lightblue"},
-    {reproduction: 3, overpopulation: 4, damage: 2, colour: "red"}
+    {reproduction: 3, overpopulation: 4, damage: 2, colour: "red"},
+    {reproduction: 2, overpopulation: 3, damage: 3, colour: "orange"}
   ];
 
   $scope.possibleNeighbours =[
